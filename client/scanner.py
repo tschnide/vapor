b840a0193c0d7cd8a83afbca03695b96dcef70a0
Travis Schnider <travis.schnider@gmail.com>
2019-01-03 00:00:38 -0700

    complete working version of scan_dir_tree

--- client/scanner.py
+++ client/scanner.py
@@ -10,13 +10,13 @@ class Scanner:
     def __init__(self, target_directory):
         self.target_directory = target_directory
 
-    def get_file_names(self):
-        for root, dirs, files in os.walk(self.target_directory):
-            for directory_name in dirs:
-                print(directory_name)
-            for file_name in files:
+    def print_dir_tree(self):
+        # the other packing is there because I don't care about the list of subdirectories.
+        for dir_path, *other, file_list in os.walk(self.target_directory, topdown=True):
+            print("Path - " + dir_path)
+            for file_name in file_list:
                 print(file_name)
 
 
-s1 = Scanner("/Users/Schnider/Desktop/vapor")
-print(s1.get_file_names())
+s1 = Scanner("/Users/Schnider/Desktop/vapor/test_root")
+s1.print_dir_tree()
